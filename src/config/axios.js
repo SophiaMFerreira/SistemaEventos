ed93fc9a3060df390641b13cc72417c3f7b8eb25
Sophia F <sophiaagnes1004@gmail.com>
2025-11-05 20:59:43 -0300

    fix: 'BASE_URL_S' route correction

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,2 +1,2 @@
-export const BASE_URL_S = 'https://my-json-server.typicode.com/SophiaMFerreira/jsonfake';
+export const BASE_URL_S = 'https://my-json-server.typicode.com/SophiaMFerreira/jsonFake';
 export const BASE_URL = 'https://my-json-server.typicode.com/castrothais/jsonfake';
