227a91ef88763f27f35cf76391ac32230a1ef9a5
SophiaMFerreira <sophiaagnes1004@gamil.com>
2025-11-21 19:24:12 -0300

    feat: add core logic to event type registration

--- src/rotas.js
+++ src/rotas.js
@@ -5,6 +5,9 @@ import ListagemEventos from './views/listagem-eventos';
 import ListagemParticipantes from './views/listagem-participantes';
 
 import CadastroTipoEvento from './views/cadastro-tipoEvento';
+import CadastroPorteEvento from './views/cadastro-porteEvento';
+import CadastroAdministrador from './views/cadastro-administrador';
+import CadastroUsuario from './views/cadastro-usuario';
 
 function Rotas(props) {
   return (
@@ -13,10 +16,12 @@ function Rotas(props) {
         <Route path='/listagem-eventos' element={<ListagemEventos />} />
         <Route path='/listagem-participantes' element={<ListagemParticipantes />} />
         
-        <Route path='/cadastro-tipoEvento' element={<CadastroTipoEvento />} />
+        <Route path='/cadastro-tipoEvento/:idParam?' element={<CadastroTipoEvento />} />
+        <Route path='/cadastro-porteEvento/:idParam?' element={<CadastroPorteEvento />} />
+        <Route path='/cadastro-administrador/:idParam?' element={<CadastroAdministrador />} />
+        <Route path='/cadastro-usuario/:idParam?' element={<CadastroUsuario />} />
       </Routes>
     </BrowserRouter>
-    
   );
 }
 
--- src/views/cadastro-tipoEvento.jsx
+++ src/views/cadastro-tipoEvento.jsx
@@ -1,20 +1,66 @@
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
+import { successMessage, errorMessage, mensagemSucesso, mensagemErro } from '../components/toastr'; 
+import Box from "@mui/material/Box";
 import Sidebar from "../components/sidebar";
 import Paper from '@mui/material/Paper';
-import Box from "@mui/material/Box";
-import Grid from '@mui/material/Grid';
 import Typography from "@mui/material/Typography";
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 
-function CadastroTipoEvento() {
+import axios from 'axios';
+import { BASE_URL } from '../config/axios';
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("Formulário enviado!");
-  };
+  function CadastroTipoEvento() {
+  const baseURL = `${BASE_URL}/tipoEvento`;
+  const { idParam } = useParams();
+  const navigate = useNavigate();
+  
+  
+  const [acao, setAcao] = React.useState("Cadastro");
+  const [mensagem, setMensagem] = React.useState("Faça cadastro de novos");
+  const [acaoButton, setAcaoButton] = React.useState("Criar");
+  const [id, setIdTipoEvento] = React.useState("");
+  const [nomeTipoEvento, setNomeTipoEvento] = React.useState("");
+  const [descricao, setDescricaoTipoEvento] = React.useState("");
+  
+  useEffect(() => {
+    if (!idParam) return;
+
+    axios.get(`${baseURL}/${idParam}`).then((response) => {
+      const dados = response.data;
+      setIdTipoEvento(dados.id);
+      setNomeTipoEvento(dados.nomeTipoEvento);
+      setDescricaoTipoEvento(dados.descricao);
+
+      setAcao("Edição");
+      setMensagem("Faça edição dos");
+      setAcaoButton("Editar");
+    });
+  }, [idParam, baseURL]);
+
+  async function save(e) {
+    e.preventDefault();
+    const data = { id, nomeTipoEvento, descricao };
+
+    try {
+      if (!idParam) {
+        await axios.post(baseURL, data);
+          mensagemSucesso(`Novo tipo ${nomeTipoEvento} criado com sucesso!`);
+          navigate(`/tela-principal`);
+      } else {
+        await axios.put(`${baseURL}/${idParam}`, data);
+          mensagemSucesso(`Tipo ${nomeTipoEvento} alterado com sucesso!`);
+          navigate(`/tela-principal`);
+      }
+      navigate("/tela-principal");
+    } catch (error) {
+      mensagemErro(error.response.data);
+    }
+  }
 
   return (
     <Box display="flex" height="100vh" bgcolor="background.default">
@@ -24,21 +70,21 @@ function CadastroTipoEvento() {
 
       <Box flex={1} p={5} overflow="auto" sx={{ position: "relative", backgroundColor: "background.default", display: "flex", flexDirection: "column", justifyContent: "space-between", }}>
         <Paper elevation={3} sx={{ p: 4 }}>
-          <Typography component="h1" variant="h3">Cadastro de Tipo de Evento</Typography>
-          <Typography variant="h6" sx={{ mb: 3 }}>Faça cadastro de novos tipos de evento.</Typography>
-          <Grid container direction="row" component="form" onSubmit={handleSubmit} noValidate sx={{justifyContent: "center", alignItems: "center", mt: 2}}>
-            <Grid item size={10} spacing={2}>
+          <Typography component="h1" variant="h3">{acao}  de Tipo de Evento</Typography>
+          <Typography variant="h6" sx={{ mb: 3 }}>{mensagem} tipos de evento.</Typography>
+          <Grid container direction="row" component="form" onSubmit={save} noValidate sx={{justifyContent: "center", alignItems: "center", mt: 2}}>
+            <Grid size={10} spacing={2}>
                 <Typography variant="h6">Nome*</Typography>
-                <TextField name="nome" placeholder="Nome do tipo de evento" required fullWidth sx={{ mb: 3, }}/>
+                <TextField name="nomeTipoEvento" placeholder="Nome do tipo de evento" required value={nomeTipoEvento} onChange={(e) => setNomeTipoEvento(e.target.value)} fullWidth sx={{ mb: 3, }}/>
             </Grid>
-            <Grid item size={10}>
+            <Grid size={10}>
                 <Typography variant="h6">Descrição</Typography>
-                <TextField name="descricao" placeholder="Descreva o evento" multiline rows={4} fullWidth sx={{ mb: 3 }}/>
+                <TextField name="descricao" placeholder="Descreva o evento" multiline rows={4} value={descricao} onChange={(e) => setDescricaoTipoEvento(e.target.value)} fullWidth sx={{ mb: 3 }}/>
             </Grid>
-            <Grid item size={10}>
+            <Grid size={10}>
                 <Stack spacing={2} direction="row">
-                    <Button variant="outlined">Voltar</Button>
-                    <Button variant="contained" type="submit">Criar Tipo de Evento</Button>
+                    <Button variant="outlined" onClick={() => navigate("/tela-principal")}>Voltar</Button>
+                    <Button variant="contained" type="submit" >{acaoButton} Tipo de Evento</Button>
                 </Stack>
             </Grid>
           </Grid>
