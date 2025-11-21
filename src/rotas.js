0a0b4bc22d2ea5ff9a0ce78f0a928184382aab2e
SophiaMFerreira <sophiaagnes1004@gamil.com>
2025-11-20 21:18:52 -0300

    feat: update routes

--- src/rotas.js
+++ src/rotas.js
@@ -4,12 +4,16 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import ListagemEventos from './views/listagem-eventos';
 import ListagemParticipantes from './views/listagem-participantes';
 
+import CadastroTipoEvento from './views/cadastro-tipoEvento';
+
 function Rotas(props) {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/listagem-eventos' element={<ListagemEventos />} />
         <Route path='/listagem-participantes' element={<ListagemParticipantes />} />
+        
+        <Route path='/cadastro-tipoEvento' element={<CadastroTipoEvento />} />
       </Routes>
     </BrowserRouter>
     
