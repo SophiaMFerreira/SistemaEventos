e68d55ecdc81c0bd8da663bb61bd5b7ef84847ee
SophiaMFerreira <sophiaagnes1004@gamil.com>
2025-11-22 15:08:24 -0300

    feat: add logic to the event port screen

--- src/views/cadastro-porteEvento.jsx
+++ src/views/cadastro-porteEvento.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
+import { mensagemSucesso, mensagemErro } from '../components/toastr'; 
 import Sidebar from "../components/sidebar";
 import Paper from '@mui/material/Paper';
 import Box from "@mui/material/Box";
@@ -8,12 +10,62 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
-function CadastroPorteEvento() {
+import axios from 'axios';
+import { BASE_URL } from '../config/axios';
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("Formulário enviado!");
-  };
+function CadastroPorteEvento() {
+  const baseURL = `${BASE_URL}/porteEvento`;
+  const { idParam } = useParams();
+  const navigate = useNavigate();
+  
+  
+  const [acao, setAcao] = React.useState("Cadastro");
+  const [mensagem, setMensagem] = React.useState("Faça cadastro de novos");
+  const [acaoButton, setAcaoButton] = React.useState("Criar");
+  const [id, setIdPorteEvento] = React.useState("");
+  const [nomePorteEvento, setNomePorteEvento] = React.useState("");
+  const [numeroMinimoParticipantes, setNumeroMinimoParticipantesPorteEvento] = React.useState(0);
+  const [numeroMaximoParticipantes, setNumeroMaximoParticipantesPorteEvento] = React.useState(0);
+  const [tempoMinimoCancelamento, setTempoMinimoCancelamentoPorteEvento] = React.useState("");
+  const [descricao, setDescricaoPorteEvento] = React.useState("");
+
+  useEffect(() => {
+    if (!idParam) return;
+
+    axios.get(`${baseURL}/${idParam}`).then((response) => {
+      const dados = response.data;
+      setIdPorteEvento(dados.id);
+      setNomePorteEvento(dados.nomePorteEvento);
+      setNumeroMinimoParticipantesPorteEvento(dados.numeroMinimoParticipantes);
+      setNumeroMaximoParticipantesPorteEvento(dados.numeroMaximoParticipantes);
+      setTempoMinimoCancelamentoPorteEvento(dados.tempoMinimoCancelamento);
+      setDescricaoPorteEvento(dados.descricao);
+
+      setAcao("Edição");
+      setMensagem("Faça edição dos");
+      setAcaoButton("Editar");
+    });
+  }, [idParam, baseURL]);
+
+  async function save(e) {
+    e.preventDefault();
+    const data = { id, nomePorteEvento, numeroMinimoParticipantes, numeroMaximoParticipantes, tempoMinimoCancelamento,  descricao };
+
+    try {
+      if (!idParam) {
+        await axios.post(baseURL, data);
+          mensagemSucesso(`Novo porte ${nomePorteEvento} criado com sucesso!`);
+          navigate(`/tela-principal`);
+      } else {
+        await axios.put(`${baseURL}/${idParam}`, data);
+          mensagemSucesso(`Porte ${nomePorteEvento} alterado com sucesso!`);
+          navigate(`/tela-principal`);
+      }
+      navigate("/tela-principal");
+    } catch (error) {
+      mensagemErro(error.response.data);
+    }
+  }
 
   return (
     <Box display="flex" height="100vh" bgcolor="background.default">
@@ -23,37 +75,37 @@ function CadastroPorteEvento() {
 
       <Box flex={1} p={5} overflow="auto" sx={{ position: "relative", backgroundColor: "background.default", display: "flex", flexDirection: "column", justifyContent: "space-between", }}>
         <Paper elevation={3} sx={{ p: 4 }}>
-          <Typography component="h1" variant="h3">Cadastro de Porte de Evento</Typography>
-          <Typography variant="h6" sx={{ mb: 3 }}>Faça cadastro de novos portes de evento.</Typography>
-          <Grid container direction="row" component="form" onSubmit={handleSubmit} noValidate sx={{justifyContent: "center", alignItems: "center", mt: 2}}>
-            <Grid item size={10}>
+          <Typography component="h1" variant="h3">{acao} de Porte de Evento</Typography>
+          <Typography variant="h6" sx={{ mb: 3 }}>{mensagem} portes de evento.</Typography>
+          <Grid container direction="row" component="form" onSubmit={save} noValidate sx={{justifyContent: "center", alignItems: "center", mt: 2}}>
+            <Grid size={10}>
                 <Typography variant="h6">Nome*</Typography>
-                <TextField name="nome" placeholder="Nome do porte de evento" required fullWidth sx={{ mb: 3, }}/>
+                <TextField name="nomePorteEvento" placeholder="Nome do porte de evento" value={nomePorteEvento} onChange={(e) => setNomePorteEvento(e.target.value)} required fullWidth sx={{ mb: 3, }}/>
             </Grid>
-            <Grid item size={10}>
+            <Grid size={10}>
               <Grid container sx={{ position: "relative", backgroundColor: "background.default", display: "flex", flexDirection: "column", justifyContent: "space-between", }}>
-                <Grid item size={6}>
+                <Grid size={6}>
                   <Typography variant="h6">Número mínimo de participantes*</Typography>
-                  <TextField name="numeroMinParticipantes" placeholder="0" min="0" type="number" required fullWidth sx={{ mb: 3 }}/>
+                  <TextField name="numeroMinimoParticipantes" placeholder="0" min="0" type="number" value={numeroMinimoParticipantes} onChange={(e) => setNumeroMinimoParticipantesPorteEvento(e.target.value)} required fullWidth sx={{ mb: 3 }}/>
                 </Grid>
-                <Grid item size={6}>
+                <Grid size={6}>
                   <Typography variant="h6">Número máximo de participantes*</Typography>
-                  <TextField name="numeroMaxParticipantes" placeholder="0" min="1" type="number" required fullWidth sx={{ mb: 3 }} />
+                  <TextField name="numeroMaximoParticipantes" placeholder="0" min="1" type="number" value={numeroMaximoParticipantes} onChange={(e) => setNumeroMaximoParticipantesPorteEvento(e.target.value)} required fullWidth sx={{ mb: 3 }} />
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item size={10} spacing={2}>
+            <Grid size={10} spacing={2}>
                 <Typography variant="h6">Tempo minimo de cancelamento*</Typography>
-                <TextField name="nome" placeholder="Tempo mínimo de cancelamento em dias" type="number" required fullWidth sx={{ mb: 3, }}/>
+                <TextField name="tempoMinimoCancelamento" placeholder="Tempo mínimo de cancelamento em dias" value={tempoMinimoCancelamento} onChange={(e) => setTempoMinimoCancelamentoPorteEvento(e.target.value)} required fullWidth sx={{ mb: 3, }}/>
             </Grid>
-            <Grid item size={10}>
+            <Grid size={10}>
                 <Typography variant="h6">Descrição</Typography>
-                <TextField name="descricao" placeholder="Descreva o porte do evento" multiline rows={4} fullWidth sx={{ mb: 3 }}/>
+                <TextField name="descricao" placeholder="Descreva o porte do evento" value={descricao} onChange={(e) => setDescricaoPorteEvento(e.target.value)} multiline rows={4} fullWidth sx={{ mb: 3 }}/>
             </Grid>
-            <Grid item size={10}>
+            <Grid size={10}>
                 <Stack spacing={2} direction="row">
-                    <Button variant="outlined">Voltar</Button>
-                    <Button variant="contained" type="submit">Criar Tipo de Evento</Button>
+                    <Button variant="outlined" onClick={() => navigate("/tela-principal")}>Voltar</Button>
+                    <Button variant="contained" type="submit">{acaoButton} Porte de Evento</Button>
                 </Stack>
             </Grid>
           </Grid>
