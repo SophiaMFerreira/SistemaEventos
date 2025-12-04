dc86ebaa18a82ff7c8c1121977de0e330f3ddee9
Thais Castro <thaiscastros29@gmail.com>
2025-12-04 09:23:31 -0300

    fix: routs

--- src/rotas.js
+++ src/rotas.js
@@ -1,17 +1,20 @@
 import React from 'react';
+
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import ListagemEventos from './views/listagem-eventos';
 import MeusEventos from './components/meus-eventos';
 import EventosOrganizados from './components/eventos-organizados';
+import CadastroEvento from './views/cadastro-eventos';
 
 
-function Rotas() {
+function Rotas(props) {
   return (
     <BrowserRouter>
       <Routes>
        <Route path="/" element={<ListagemEventos />} />
+       <Route path='/cadastro-eventos/:idParam?' element={<CadastroEvento />} />
       <Route path="/meus-eventos/:idParam?" element={<MeusEventos/>} />
-      <Route path="/eventos-organizados" element={<EventosOrganizados />} />
+      <Route path="/eventos-organizados/:idParam?" element={<EventosOrganizados />} />
       </Routes>
     </BrowserRouter>
   );
