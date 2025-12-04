0f4137d12ac2b40d22664894ff95dad61a680d17
Thais Castro <thaiscastros29@gmail.com>
2025-12-04 09:23:15 -0300

    feat: filter event by name my events

--- src/components/meus-eventos.js
+++ src/components/meus-eventos.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
+import BuscarEvento from "./input-buscar-evento";
 
 function MeusEventos() {
   const idParticipante = Number(localStorage.getItem("idUsuario"));
   const [eventosInscritos, setEventosInscritos] = useState([]);
+  const [filtro, setFiltro] = React.useState("");
+  
+  const eventosFiltrados = eventosInscritos.filter(ev =>
+  ev.nomeEvento.toLowerCase().includes(filtro.toLowerCase())
+    );
+
 
   useEffect(() => {
     async function carregarEventos() {
@@ -39,15 +46,27 @@ function MeusEventos() {
     <div>
       <h2>Meus Eventos</h2>
 
-      {eventosInscritos.length === 0 ? (
+       <BuscarEvento
+            value={filtro}
+            onChange={setFiltro}
+            placeholder="Digite nome do evento"
+          />
+
+      {eventosFiltrados.length === 0 ? (
         <p>Você ainda não está inscrito em nenhum evento.</p>
       ) : (
         <ul>
-          {eventosInscritos.map(ev => (
-            <li key={ev.id}>
-              {ev.nomeEvento} — {ev.dataInicio}
-            </li>
-          ))}
+          {eventosFiltrados.map(ev => {
+            const data = new Date(ev.dataInicio);
+            const dia = String(data.getDate()).padStart(2, "0");
+            const mes = data.toLocaleString("pt-BR", { month: "long" });
+
+            return (
+              <li key={ev.id}>
+                <strong>{ev.nomeEvento}</strong> — {dia} de {mes} — {ev.cidade}
+              </li>
+            );
+          })}
         </ul>
       )}
     </div>
