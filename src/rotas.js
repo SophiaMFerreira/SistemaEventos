8c36a7fd9069f1d51d70c461d34754ac4aa8fd2e
Thais Castro <thaiscastros29@gmail.com>
2025-12-13 17:39:31 -0300

    feature: issue ticket

--- src/rotas.js
+++ src/rotas.js
@@ -5,6 +5,7 @@ import ListagemEventos from './views/listagem-eventos';
 import MeusEventos from './components/meus-eventos';
 import EventosOrganizados from './components/eventos-organizados';
 import CadastroEvento from './views/cadastro-eventos';
+import ListagemIngressos from './views/listagem-ingresso';
 
 
 function Rotas(props) {
@@ -13,8 +14,9 @@ function Rotas(props) {
       <Routes>
        <Route path="/" element={<ListagemEventos />} />
        <Route path='/cadastro-eventos/:idParam?' element={<CadastroEvento />} />
-      <Route path="/meus-eventos/:idParam?" element={<MeusEventos/>} />
+      <Route path="/meus-eventos" element={<MeusEventos/>} />
       <Route path="/eventos-organizados/:idParam?" element={<EventosOrganizados />} />
+      <Route path="/meus-eventos/:idEvento/ingresso" element={<ListagemIngressos />} />
       </Routes>
     </BrowserRouter>
   );
