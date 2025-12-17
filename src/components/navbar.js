1a6b4021e74b5cfdd2b735a762683c343361f673
SophiaMFerreira <sophiaagnes1004@gamil.com>
2025-12-17 10:55:42 -0300

    feat: profile-adaptative navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 import 'bootswatch/dist/flatly/bootstrap.css';
 
 import NavbarItem from './navbarItem';
 
+import axios from 'axios';
+import { BASE_URL_S } from '../config/axios';
+
 function Navbar(props) {
+    const [organizador, setOrganizador] = useState(false);
+    const [url, setUrl] = useState("/cadastro-administrador");
+    const [idParticipante, setIdParticipante] = useState(null);
+
+    useEffect(() => {
+        const id = Number(localStorage.getItem("idUsuario"));
+        const tipoParticipante = localStorage.getItem("tipoParticipante");
+
+        if (!id || !tipoParticipante) return;
+        setIdParticipante(id);
+
+        if (tipoParticipante === "admin") {
+          setOrganizador(true);
+          setUrl("/cadastro-administrador");
+          return;
+        }
+
+        let baseURL = "";
+        if (tipoParticipante === "cpf") {
+          baseURL = `${BASE_URL_S}/participanteCPF`;
+          setUrl("/cadastro-usuarioCPF");
+        }
+        if (tipoParticipante === "cnpj") {
+          baseURL = `${BASE_URL_S}/participanteCNPJ`;
+          setUrl("/cadastro-usuarioCNPJ");
+        }
+        axios.get(`${baseURL}/${id}`)
+          .then((response) => {
+            setOrganizador(response.data.organizador);
+          })
+          .catch(() => {
+            setOrganizador(false);
+          });
+      }, []); 
+
   return (
     <div className='navbar navbar-expand-lg fixed-top navbar-dark bg-primary'>
       <div className='container'>
@@ -24,7 +62,7 @@ function Navbar(props) {
         <div className='collapse navbar-collapse' id='navbarResponsive'>
           
           <ul className='navbar-nav'>
-            <NavbarItem render='true' href='/eventos-organizados' label='Sou organizador' />
+            <NavbarItem render={organizador} href='/eventos-organizados' label='Sou organizador' />
           </ul>
 
           <ul className='navbar-nav'>
@@ -32,11 +70,11 @@ function Navbar(props) {
           </ul>
 
           <ul className='navbar-nav'>
-            <NavbarItem render='true' href='/meus-certificados' label='Meus Certificados' />
+            <NavbarItem render='true' href={`${url}/${idParticipante}`} label='Perfil' />
           </ul>
-          
+
           <ul className='navbar-nav'>
-            <NavbarItem render='true' href='/' label='Entrar' />
+            <NavbarItem render='true' href='/' label='Sair' />
             </ul>
         </div>
       </div>
