facab6773f1fc47cd469dc1677e02389c8f37b14
SophiaMFerreira <sophiaagnes1004@gamil.com>
2026-06-14 17:57:27 -0300

    fix: integration fixes in 'tipo de evento'

--- src/views/cadastro-tipoEvento.jsx
+++ src/views/cadastro-tipoEvento.jsx
@@ -13,22 +13,22 @@ function CadastroTipoEvento() {
   const { idParam } = useParams();
   const navigate = useNavigate();
   
-  
   const [acao, setAcao] = useState("Cadastro");
   const [mensagem, setMensagem] = useState("Faça cadastro de novos");
   const [acaoButton, setAcaoButton] = useState("Criar");
-  const [id, setIdTipoEvento] = useState("");
-  const [nomeTipoEvento, setNomeTipoEvento] = useState("");
-  const [descricao, setDescricaoTipoEvento] = useState("");
+
+  const [id, setId] = useState("");
+  const [nome, setNome] = useState("");
+  const [descricao, setDescricao] = useState("");
   
   useEffect(() => {
     if (!idParam) return;
 
     axios.get(`${baseURL}/${idParam}`).then((response) => {
       const dados = response.data;
-      setIdTipoEvento(dados.id);
-      setNomeTipoEvento(dados.nomeTipoEvento);
-      setDescricaoTipoEvento(dados.descricao);
+      setId(dados.id);
+      setNome(dados.nome);
+      setDescricao(dados.descricao);
 
       setAcao("Edição");
       setMensagem("Faça edição dos");
@@ -38,21 +38,21 @@ function CadastroTipoEvento() {
 
   async function save(e) {
     e.preventDefault();
-    const data = { id, nomeTipoEvento, descricao };
+    const data = { id, nome, descricao };
 
     try {
-      if (!idParam) {
+      if (!idParam) {      
         await axios.post(baseURL, data);
-          mensagemSucesso(`Novo tipo ${nomeTipoEvento} criado com sucesso!`);
+          mensagemSucesso(`Novo tipo ${nome} criado com sucesso!`);
           navigate(`/listagem-eventos`);
       } else {
         await axios.put(`${baseURL}/${idParam}`, data);
-          mensagemSucesso(`Tipo ${nomeTipoEvento} alterado com sucesso!`);
+          mensagemSucesso(`Tipo ${nome} alterado com sucesso!`);
           navigate(`/listagem-eventos`);
       }
       navigate("/listagem-eventos");
     } catch (error) {
-      mensagemErro(error.response.data);
+      mensagemErro(error?.response?.data || "Erro ao salvar os dados.");
     }
   }
 
@@ -64,11 +64,11 @@ function CadastroTipoEvento() {
               headers: { 'Content-Type': 'application/json' },
           })
           .then(function (response) {
-              mensagemSucesso(`Tipo de evento ${nomeTipoEvento} excluído com sucesso!`);
+              mensagemSucesso(`Tipo de evento ${nome} excluído com sucesso!`);
               navigate(`/listagem-eventos`);
           })
           .catch(function (error) {
-              mensagemErro(`Erro ao excluir ${nomeTipoEvento}`);
+              mensagemErro(`Erro ao excluir ${nome}`);
           });
   }
 
@@ -80,11 +80,11 @@ function CadastroTipoEvento() {
                   <Grid container component="form" onSubmit={save} noValidate spacing={2} >
                        <Grid size={12} sx={{ mb: 2, mx: 2, width: "100%"}}>
                           <Typography variant="body1" className="label">Nome*</Typography>
-                          <TextField name="nomeTipoEvento" placeholder="Nome do tipo de evento" required value={nomeTipoEvento} onChange={(e) => setNomeTipoEvento(e.target.value)} fullWidth/>
+                          <TextField name="nome" placeholder="Nome do tipo de evento" required value={nome} onChange={(e) => setNome(e.target.value)} fullWidth/>
                       </Grid>
                        <Grid size={12} sx={{ mb: 2, mx: 2, width: "100%"}}>
                           <Typography variant="body1" className="label">Descrição</Typography>
-                          <TextField name="descricao" placeholder="Descreva o evento" multiline rows={4} value={descricao} onChange={(e) => setDescricaoTipoEvento(e.target.value)} fullWidth/>
+                          <TextField name="descricao" placeholder="Descreva o evento" multiline rows={4} value={descricao} onChange={(e) => setDescricao(e.target.value)} fullWidth/>
                       </Grid>
                       <Grid item xs={12} justifyContent="flex-end">
                           <Stack spacing={2} direction={{ xs: "column", sm: "row" }} justifyContent="flex-end" >
